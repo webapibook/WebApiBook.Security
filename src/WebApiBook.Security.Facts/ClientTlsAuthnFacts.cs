854b76a0c8f67adc8895bbc94bb332bc6dd3921e
Pedro Felix <pmhsfelix@gmail.com>
2013-11-16 20:06:41 +0000

    Refactorings and test additions

--- src/WebApiBook.Security.Facts/ClientTlsAuthnFacts.cs
+++ src/WebApiBook.Security.Facts/ClientTlsAuthnFacts.cs
@@ -8,6 +8,7 @@ using System.ServiceModel;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web.Http;
 using System.Web.Http.Hosting;
 using System.Web.Http.SelfHost;
 using WebApiBook.Security.Common;
@@ -15,8 +16,43 @@ using Xunit;
 
 namespace WebApiBook.Security.Facts
 {
+    public class HelloController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            //var clientCert = Request.GetClientCertificate();
+            var clientCert = Request.GetRequestContext().ClientCertificate;
+            var clientName = clientCert == null ? "stranger" : clientCert.Subject;
+            return new HttpResponseMessage
+            {
+                Content = new StringContent("Hello there, " + clientName)
+            };
+        }
+    }
+
     public class ClientTlsAuthnFacts
     {
+        [Fact]
+        public async Task Actions_can_get_the_client_certificate()
+        {
+            var config = new HttpSelfHostConfiguration("https://www.example.net:8443");
+            config.ClientCredentialType = HttpClientCredentialType.Certificate;
+            config.Routes.MapHttpRoute(
+                "ApiDefault",
+                "{controller}/{id}",
+                new {id=RouteParameter.Optional }
+                );
+            var server = new HttpSelfHostServer(config);
+            server.OpenAsync().Wait();
+            using (var client = new HttpClient(new HttpClientHandler() { ClientCertificateOptions = ClientCertificateOption.Automatic }))
+            {
+                var resp = await client.GetAsync("https://www.example.net:8443/hello");
+                Assert.Equal(HttpStatusCode.OK, resp.StatusCode);
+                var body = await resp.Content.ReadAsStringAsync();
+                Assert.False(body.Contains("stranger"));
+            }
+        }
+
         [Fact]
         public async Task SomeFact()
         {
